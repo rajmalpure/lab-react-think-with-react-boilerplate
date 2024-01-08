46c3eed8b4ddc97c4abad87c00708e410f9f1c8a
rajmalpure <malpure.raj@kalvium.community>
2024-01-08 11:31:20 +0530

    jsx added

--- Progression 3/ReactApp/src/App.jsx
+++ Progression 3/ReactApp/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          <h1>Hello Kalvium.</h1>
         </p>
       </div>
       <p className="read-the-docs">
